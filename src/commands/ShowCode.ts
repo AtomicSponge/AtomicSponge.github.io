1f45fee974bfeaedd5ced4c589deaf15732f92e9
Matthew Evans <mevans+git@wtfsystems.net>
2024-05-08 20:21:16 -0400

    sync

--- src/commands/ShowCode.ts
+++ src/commands/ShowCode.ts
@@ -56,6 +56,6 @@ export class ShowCode extends Command {
       }
     })
     if(res !== '') return res
-    return `Nothing found with effect name ${args[0]}\n${this.help}`
+    return `Nothing found with name ${args[0]}\n${this.help}`
   }
 }
