83b8f46319d4549fbd9412a562ebfc6d05db3d94
Matthew Evans <mevans+git@wtfsystems.net>
2024-05-31 14:25:34 -0400

    sync

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import prism from 'vite-plugin-prismjs'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue(), cssInjectedByJsPlugin(),
     prism({
       languages: [
         'javascript', 'typescript', 'cpp', 'cmake'
